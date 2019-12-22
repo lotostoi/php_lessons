b9efc3e7f5be29cd9d5857982e653e7e47b17d4b
Alexander <lotos_toi@mail.ru>
2019-12-23 01:50:29 +1000

    Update index.php

--- minimalistica/index.php
+++ minimalistica/index.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Php lesson1';
-$h1 = 'Hello PHP'." ". date('o');
+$h1 = 'Hello PHP!!'." ". date('o');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
